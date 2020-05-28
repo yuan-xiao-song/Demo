5c3dec45854fabb55058817ad8b9e9cda10a50f2
袁晓松 <xsyuan@liangzhicn.com>
2020-05-28 13:56:31 +0800

    'm'

--- run.py
+++ run.py
@@ -5,7 +5,5 @@ import pytest
 from common.filepath import REPORTDIR
 
 if __name__ == '__main__':
-    allure_report_path = REPORTDIR + '/allure_report'
-    html_report_path = REPORTDIR + '/html'
-    pytest.main(['-s', '-q', "--alluredir={}".format(allure_report_path)])
-    # os.system('allure generate {} -o {} --clean'.format(allure_report_path, html_report_path))
+    pytest.main(['-s', '-q', "--alluredir=allure_report"])
+
