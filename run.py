8705a6a182df88169e41d4f8fe0b70c706927198
袁晓松 <xsyuan@liangzhicn.com>
2020-05-28 11:51:36 +0800

    update

--- run.py
+++ run.py
@@ -8,4 +8,4 @@ if __name__ == '__main__':
     allure_report_path = REPORTDIR + '/allure_report'
     html_report_path = REPORTDIR + '/html'
     pytest.main(['-s', '-q', "--alluredir={}".format(allure_report_path)])
-    os.system('allure generate {} -o {} --clean'.format(allure_report_path, html_report_path))
+    # os.system('allure generate {} -o {} --clean'.format(allure_report_path, html_report_path))
